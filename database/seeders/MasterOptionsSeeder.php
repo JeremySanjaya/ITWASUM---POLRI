af370eb60baa6414676a2013fc4a879a154da84e
Firmansyah <admin@tigapilarmandiri.com>
2026-03-18 11:05:02 +0800

    feat: add master table migration and seeder

---
+++ database/seeders/MasterOptionsSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class MasterOptionsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('master_options')->insert([
+            [
+                'id' => 6,
+                'kategori' => 'struktural',
+                'nama_opsi' => 'Irwil I',
+                'created_at' => Carbon::parse('2026-03-17 08:27:04'),
+                'updated_at' => Carbon::parse('2026-03-17 08:27:04'),
+            ],
+            [
+                'id' => 7,
+                'kategori' => 'struktural',
+                'nama_opsi' => 'Iriwil II',
+                'created_at' => Carbon::parse('2026-03-17 08:27:44'),
+                'updated_at' => Carbon::parse('2026-03-17 08:27:44'),
+            ],
+            [
+                'id' => 8,
+                'kategori' => 'struktural',
+                'nama_opsi' => 'Irwil III',
+                'created_at' => Carbon::parse('2026-03-17 08:27:51'),
+                'updated_at' => Carbon::parse('2026-03-17 08:27:51'),
+            ],
+        ]);
+    }
+}
